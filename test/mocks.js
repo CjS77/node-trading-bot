6ed3646870676359eb984887f86d2019780af505
Cayle Sharrock <cayle@coinbase.com>
2016-04-29 14:27:33 +0200

    Initial attempt

---
+++ test/mocks.js
@@ -0,0 +1,112 @@
+'use strict';
+
+var mockOrderBook = {
+    sequence: '3',
+    'bids': [
+        ['402.05', '1', 2],
+        ['402.00', '2', 1],
+        ['401.00', '1', 2],
+        ['400.00', '5', 3],
+        ['398.00', '1', 1]
+    ],
+    'asks': [
+        ['403.05', '1', 1],
+        ['404.00', '2', 2],
+        ['406.00', '3', 3],
+        ['406.00', '3', 6],
+        ['410.00', '5', 5],
+    ]
+};
+
+class MockAuthenticatedClient {
+    constructor() {
+        this.orders = [
+            {
+                "id": "d50ec984-77a8-460a-b958-66f114b0de9b",
+                "size": "0.5",
+                "price": "400.00",
+                "product_id": "BTC-USD",
+                "status": "open",
+                "filled_size": "0.00",
+                "fill_fees": "0.001",
+                "settled": false,
+                "side": "buy",
+                "created_at": "2014-11-14T06:39:55.000000Z"
+            },
+            {
+                "id": "d50ec984-77a8-460a-b958-66f114b0de9c",
+                "size": "0.8",
+                "price": "401.00",
+                "product_id": "BTC-USD",
+                "status": "open",
+                "filled_size": "0.00",
+                "fill_fees": "0.001",
+                "settled": false,
+                "side": "buy",
+                "created_at": "2014-11-14T06:39:56.000000Z"
+            },
+            {
+                "id": "d50ec984-77a8-460a-b958-66f114b0de9d",
+                "size": "1",
+                "price": "410.00",
+                "product_id": "BTC-USD",
+                "status": "open",
+                "filled_size": "0.00",
+                "fill_fees": "0.001",
+                "settled": false,
+                "side": "sell",
+                "created_at": "2014-11-14T06:39:57.000000Z"
+            }
+        ]
+    }
+
+    getOrders() {
+        return this.orders;
+    }
+
+    cancelOrder(id, cb) {
+        if (id == null) {
+            this.orders = [];
+            return cb(null, true);
+        }
+        var order = null;
+        for (let i = 0; i < this.orders.length; i++) {
+            order = this.orders[i];
+            if (order.id === id) {
+                this.orders.splice(i, 1)
+                break;
+            }
+        }
+        cb(null, order ? id : null);
+    }
+
+    getProductTicker(cb) {
+        cb(null, {
+            "trade_id": 0,
+            "price": "402.50",
+            "size": "0.5",
+            "bid": "402.05",
+            "ask": "403.05",
+            "volume": "5957.11914015",
+            "time": "2015-11-14T20:46:03.511254Z"
+        })
+    }
+
+    getProductOrderBook(options, cb) {
+        cb(null, mockOrderBook);
+    }
+
+    getOrders(cb) {
+        cb(null, this.orders);
+    }
+}
+
+class MockWebsocketClient {
+
+}
+
+module.exports = {
+    AuthenticatedClient: MockAuthenticatedClient,
+    WebsocketClient: MockWebsocketClient,
+    mockOrderBook: mockOrderBook
+};
---
+++ test/trading_bot_test.js
@@ -0,0 +1,155 @@
+'use strict';
+const expect = require('expect.js');
+const TradingBot = require('../lib/trading_bot');
+const mocks = require('./mocks');
+
+describe('The TradingBot class', () => {
+    var bot;
+
+    beforeEach(() => {
+        let options = {
+            client: new mocks.AuthenticatedClient(),
+            websocketClient: new mocks.WebsocketClient()
+        };
+        bot = new TradingBot(options);
+    });
+
+    it('can be constructed', () => {
+        expect(bot instanceof TradingBot).to.be(true);
+    });
+    
+    it('requires an exchange client', () => {
+        expect(() => new TradingBot()).to.throwError();
+    });
+    
+
+    it('starts off not trading', () => {
+        expect(bot.is_trading).to.equal(false);
+    });
+
+    it('sets the is_trading flag', () => {
+        return bot.start_trading().then(() => {
+            expect(bot.is_trading).to.be(true);
+            return bot.stop_trading();
+        }).then(() => {
+            expect(bot.is_trading).to.be(false);
+        });
+    });
+
+    it('the ticker is initially undefined', () => {
+        expect(bot.ticker.updated).to.be(undefined);
+        expect(bot.ticker.data).to.be(undefined);
+    });
+
+    it('the price is initially undefined', () => {
+        expect(bot.price.updated).to.be(undefined);
+        expect(bot.price.data).to.be(undefined);
+    });
+
+    it('the ticker is initially undefined', () => {
+        expect(bot.midmarket_price.updated).to.be(undefined);
+        expect(bot.midmarket_price.data).to.be(undefined);
+    });
+
+    it('the order book is initially undefined', () => {
+        expect(bot.order_book.updated).to.be(undefined);
+        expect(bot.order_book.data).to.be(undefined);
+    });
+
+    it('the ticker price is initially undefined', () => {
+        expect(bot.order_book.updated).to.be(undefined);
+        expect(bot.order_book.data).to.be(undefined);
+    });
+
+    it('my orders are initially undefined', () => {
+        expect(bot.my_orders.updated).to.be(undefined);
+        expect(bot.my_orders.data).to.be(undefined);
+    });
+
+    it('provides data after a refresh', (done) => {
+        bot.refresh_indicators();
+        setTimeout(() => {
+            expect(bot.ticker.data.ask).to.equal('403.05');
+            expect(bot.price.data).to.equal('402.50');
+            expect(bot.order_book.data).to.eql(mocks.mockOrderBook);
+            expect(bot.my_orders.data).to.eql(bot.client.orders);
+            expect(bot.midmarket_price.data).to.equal(402.55);
+            done();
+        }, 50);
+    });
+    
+    it('can cancel an order', (done) => {
+        bot.cancel_order('d50ec984-77a8-460a-b958-66f114b0de9c').then(() => {
+            bot.refresh_indicators();
+            setTimeout(() => {
+                expect(bot.my_orders.data.length).to.equal(2);
+                expect(bot.my_orders.data[0].id).to.equal('d50ec984-77a8-460a-b958-66f114b0de9b');
+                expect(bot.my_orders.data[1].id).to.equal('d50ec984-77a8-460a-b958-66f114b0de9d');
+                done();
+            }, 50);
+        });
+    });
+
+    it('can cancel all orders', (done) => {
+        bot.cancel_all_orders().then(() => {
+            bot.refresh_indicators();
+            setTimeout(() => {
+                expect(bot.my_orders.data).to.eql([]);
+                console.log('All orders deleted');
+                done();
+            }, 50);
+        });
+    });
+});
+
+describe('A simple bot', () => {
+    class SimpleBot extends TradingBot {
+        constructor() {
+            super({
+                client: new mocks.AuthenticatedClient()
+            });
+            this.count = 0;
+        }
+
+        _execute_trading_strategy() {
+            this.count++;
+            return Promise.resolve();
+        }
+    }
+
+    var bot;
+    beforeEach(() => {
+        bot = new SimpleBot();
+    });
+    it('can be subclassed from TradingBot', () => {
+        expect(bot instanceof SimpleBot).to.be(true);
+        expect(bot instanceof TradingBot).to.be(true);
+    });
+
+    it('executes execution script', (done) => {
+        setTimeout(() => {
+            console.log('Got here too', bot.count);
+            expect(bot.count).to.be.greaterThan(1);
+            done();
+        }, 20);
+        bot.start_trading({ interval: 5 }).then(() => {
+            console.log('Got here');
+            expect(bot.is_trading).to.be(true);
+            expect(bot.count).to.be(0);
+        });
+    });
+    
+    it('logs api errors', (done) => {
+        const old_ticker = bot.client.getProductTicker;
+        bot.client.getProductTicker = (cb) => { cb('Could not get ticker') };
+        bot.refresh_indicators();
+        setTimeout(() => {
+            expect(bot.ticker.errors.length).to.be(1);
+            expect(bot.ticker.errors[0].error).to.be('Could not get ticker');
+            bot.client.getProductTicker = old_ticker;
+            done();
+        }, 20)
+    }
+    )
+});
+
